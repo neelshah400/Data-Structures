211fb573c6c8baf4bea2ae4caa65e81678331447
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2020-09-22 11:12:56 -0400

    Spiraling Task

--- IntroTextFiles/src/Spiraling.java
+++ IntroTextFiles/src/Spiraling.java
@@ -11,18 +11,59 @@ public class Spiraling {
             BufferedReader input = new BufferedReader(new FileReader(file));
             String text;
             while ((text = input.readLine()) != null) {
+
                 int n = Integer.parseInt(text);
                 String[][] array = new String[n][n];
 
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (array[i][j] == null)
-                            array[i][j] = "-";
-                        System.out.print(array[i][j] + " ");
+                for (int row = 0; row < n; row++) {
+                    for (int col = 0; col < n; col++)
+                        array[row][col] = "-";
+                }
+
+                int startRow = 0;
+                int startCol = 0;
+                int endRow = n - 1;
+                int endCol = n - 1;
+
+                while (startRow <= endRow && startCol <= endCol) {
+
+                    // right
+                    for (int col = startCol; col <= endCol; col++)
+                        array[startRow][col] = "*";
+                    startRow++;
+                    if (startCol >= 1)
+                        startCol++;
+
+                    // down
+                    for (int row = startRow; row <= endRow; row++)
+                        array[row][endCol] = "*";
+                    endCol--;
+                    startRow++;
+
+                    // left
+                    for(int col = endCol; col >= startCol; col--)
+                        array[endRow][col] = "*";
+                    endRow--;
+                    endCol--;
+
+                    // up
+                    for (int row = endRow; row >= startRow; row--)
+                        array[row][startCol] = "*";
+                    startCol++;
+                    endRow--;
+
+                }
+
+                if (n % 4 == 2)
+                    array[n / 2][(n - 1) / 2] = "-";
+
+                for (int row = 0; row < n; row++) {
+                    for (int col = 0; col < n; col++) {
+                        System.out.print(array[row][col] + "  ");
                     }
                     System.out.println();
                 }
-                System.out.println();
+
             }
         } catch (IOException e) {
             System.out.println("File not found.");
